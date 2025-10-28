cad912f985ce1858769302256d00a1e87a79b37e
franciscoalfaro <franciscoalfar@gmail.com>
2025-10-28 13:59:58 -0300

    new module

---
+++ modules/reports/reports.controller.js
@@ -0,0 +1,9 @@
+//aca se realizar la generacion de reportes calcular el tiempo o sla en el cual el ticket se mantiene abierto hasta que se cierra
+//Resolucion del primer contacto, relacionado en caso de que usuario abra nuevo ticket bajo la misma incidencia. 
+//Total de ticket cerrados
+//cuantos ticket fueron creados por dia durante los ultimos 7 dias. 
+//Ticket totales
+//Ticket abiertos
+//Ticket sin asignar
+//Ticket pendientes. 
+//Ticket por area(departamento)
